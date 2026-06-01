844fe280e64801dcacd0462d65b4570052e4754c
Gagan <gaganrdev@gmail.com>
2026-06-01 16:28:43 +0530

    Initial Commit

---
+++ AudioMesh/AudioMeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMeshApp.swift
+//  AudioMesh
+//
+//  Created by Gagan R on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioMeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
